dd595f135f98c1de0834e5aaa652864cd46bd00d
github-mfcaas <139184777+github-mfcaas@users.noreply.github.com>
2023-09-09 20:28:20 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'MFCaaS'
+copyright = '2023, MFCaaS'
+author = 'MFCaaS Team'
 
 release = '0.1'
 version = '0.1.0'
